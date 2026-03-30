d3c632334d57ac4631a89be33dca4ebac30a75b5
kruily <1683196883@qq.com>
2026-03-30 17:19:45 +0800

    feat(server): 更新欢迎消息内容
    - 将响应消息从 "Hello CI/CD" 修改为 "Hello CI/CD，Hello Guys!"

--- main.go
+++ main.go
@@ -30,7 +30,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello CI/CD")
+		fmt.Fprintf(w, "Hello CI/CD，Hello Guys!")
 	})
 
 	http.ListenAndServe(":8080", nil)
